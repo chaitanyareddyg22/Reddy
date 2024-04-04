5bec565c33ed7d8ae673f4cddde6f70cfc187e51
chaitanyareddyg22 <59408711+chaitanyareddyg22@users.noreply.github.com>
2024-04-04 15:13:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,15 @@ if app_mode == 'Home':
   st.markdown('Applicant Income VS Loan Amount ')
   st.bar_chart(data[['ApplicantIncome','LoanAmount']].head(20))
 
+  col1, col2 = st.columns([3, 1])
+  data = np.random.randn(10, 1)
+  
+  col1.subheader("A wide column with a chart")
+  col1.line_chart(data)
+  
+  col2.subheader("A narrow column with the data")
+  col2.write(data)
+
 elif app_mode == 'Prediction':
   # st.image('slider-short-3.jpg')
   st.subheader('Sir/Mme , YOU need to fill all necessary informations in order    to get a reply to your loan request !')
